ad620473ea3d037d63e27c51886357047784a6d1
Adam <horada1991@gmail.com>
2016-05-11 12:40:56 +0200

    Fixes in piton.py

--- piton.py
+++ piton.py
@@ -1,10 +1,11 @@
 #Moving Snake's parts' coordinates
 import curses
 screen = curses.initscr()
+dims = screen.getmaxyx()
 
-
-def MovingCoords(CoordList, y, x, edge_y, edge_x):
-    if y < edge_y and x < edge_x and (x not in CoordList[2:] and y not in CoordList[2:]):
+def MovingCoords(CoordList, y, x):
+    global dims
+    if y < dims[0]-1 and x < dims[1]-1 and (x not in CoordList[2:] and y not in CoordList[2:]):
         for element in range(len(CoordList)-1, 1, -1):
             CoordList[element] = CoordList[element-2]
         CoordList[0] = y
@@ -13,7 +14,7 @@ def MovingCoords(CoordList, y, x, edge_y, edge_x):
     else:
         return 0
 
-
+#If eat food, than grow in length
 def Eat(CoordList, food_y, food_x):
     if CoordList[0] == food_y and CoordList[1] == food_x:
         if CoordList[-2] == CoordList[-4]:
@@ -24,20 +25,25 @@ def Eat(CoordList, food_y, food_x):
     else:
         return 0
 
+#del snake
 def DelSnake(CoordList):
     global screen
-    for i in range(0, len(CoordList) - 1):
-        screen.delch(CoordList[0], CoordList[1])
+    for i in range(0, len(CoordList) - 1, 2):
+        screen.delch(CoordList[i], CoordList[i + 1])
+        screen.addch(CoordList[i], CoordList[i + 1], " ")
 
+#print snake on new coords
 def PrintSnake(CoordList):
     global screen
     for i in range(0, len(CoordList)-1, 2):
         screen.addch(CoordList[i], CoordList[i+1], "@")
 
+
+
 #TEST LINES
-"""List1 = [2, 1, 2, 2, 2, 3, 2, 4, 2, 5]
+ist1 = [2, 1, 2, 2, 2, 3, 2, 4, 2, 5]
 PrintSnake(List1)
 screen.getch()
 DelSnake(List1)
 screen.getch()
-curses.endwin()"""
+curses.endwin()
