63578988eaf789c8345c841efe21eb0c970fcaa8
Adam <horada1991@gmail.com>
2016-05-11 14:52:33 +0200

    DerpSnake.py changed -- Snake is movinggit add .!

--- DerpSnake.py
+++ DerpSnake.py
@@ -10,34 +10,33 @@ screen.nodelay(1)
 screen.keypad(1)
 dims = screen.getmaxyx()
 
-Coords = [int(dims[0] / 2), int(dims[1] / 2), int(dims[0] / 2), int(dims[1] / 2-1),
-                int(dims[0] / 2), int(dims[1] / 2-2)]
+Coords = [4, 13, 4, 12, 4, 11, 4, 10]
 move_y = 0
-move_x = 0
+move_x = 1
 q = -1
 screen.border()
-screen.addstr(0, 5, "   Score:   ")
 
 while q != ord("q"):
     q = screen.getch()
     if q == curses.KEY_UP:
-        move_y, move_x = 1, 0
-    elif q == curses.KEY_DOWN:
         move_y, move_x = -1, 0
+    elif q == curses.KEY_DOWN:
+        move_y, move_x = 1, 0
     elif q == curses.KEY_LEFT:
         move_y, move_x = 0, -1
     elif q == curses.KEY_RIGHT:
         move_y, move_x = 0, 1
     y_head = Coords[0] + move_y
     x_head = Coords[1] + move_x
-    piton.DelSnake(Coords)
     Coords = piton.MovingCoords(Coords, y_head, x_head)
     if Coords == 0:
         screen.addstr(5, 5, "GAME OVER")
         q = ord("q")
     else:
         piton.PrintSnake(Coords)
-        piton.Eat(Coords, x_head, y_head)
+        screen.border()
+        screen.addstr(0, 5, "   Score:   ")
+        piton.Eat(Coords, 80, 80)
         time.sleep(0.1)
 
 screen.nodelay(0)
--- piton.py
+++ piton.py
@@ -5,7 +5,10 @@ dims = screen.getmaxyx()
 #Moving Snake's parts' coordinates
 def MovingCoords(CoordList, y, x):
     global dims
-    if (y < dims[0] and x < dims[1]) and (x not in CoordList[2:] or y not in CoordList[2:]):
+    if y < dims[0] and x < dims[1]:
+        for i in range(2, len(CoordList)-1, 2):
+            if CoordList[i] == y and CoordList[i+1] == x:
+                return 0
         for element in range(len(CoordList)-1, 1, -1):
             CoordList[element] = CoordList[element-2]
         CoordList[0] = y
@@ -35,15 +38,14 @@ def DelSnake(CoordList):
 #print snake on new coords
 def PrintSnake(CoordList):
     global screen
+    screen.clear()
     for i in range(0, len(CoordList)-1, 2):
         screen.addch(CoordList[i], CoordList[i+1], "@")
 
 
 
-#TEST LINES
-"""ist1 = [2, 1, 2, 2, 2, 3, 2, 4, 2, 5]
-PrintSnake(List1)
-screen.getch()
-DelSnake(List1)
-screen.getch()
+"""TEST LINES
+dims = (10, 15)
+List1 = [2, 5, 2, 4, 2, 3, 2, 2, 2, 1]
+print(MovingCoords(List1, 2, 6))
 curses.endwin()"""
