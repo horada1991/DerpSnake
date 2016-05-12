73d061500d8c82ad7d109140c687a5021542b913
Adam <horada1991@gmail.com>
2016-05-12 09:15:24 +0200

    No food bug handeled

--- DerpSnake.py
+++ DerpSnake.py
@@ -23,6 +23,11 @@ while q != ord("q"):
     screen.addch(FoodCoords[0], FoodCoords[1], "✪")
     food_y = FoodCoords[0]
     food_x = FoodCoords[1]
+    EatCoords = piton.Eat(Coords, food_y, food_x)
+    if EatCoords != 0:
+        screen.addch(Coords[-2], Coords[-1], "▪")
+        FoodCoords = environment.Food()
+        Coords = EatCoords
     if q == curses.KEY_UP and move_y != 1:
         move_y, move_x = -1, 0
     elif q == curses.KEY_DOWN and move_y != -1:
@@ -43,11 +48,6 @@ while q != ord("q"):
         screen.addch(FoodCoords[0], FoodCoords[1], "✪")
         piton.PrintSnake(Coords)
         screen.border()
-        EatCoords = piton.Eat(Coords, food_y, food_x)
-        if EatCoords != 0:
-            screen.addch(Coords[-2], Coords[-1], "▪")
-            FoodCoords = environment.Food()
-            Coords = EatCoords
     time.sleep(0.1)
 
 screen.nodelay(0)
